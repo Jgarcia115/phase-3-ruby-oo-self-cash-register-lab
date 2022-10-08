612a1ffeba536bf140e1e0438a728f0514c7c766
Jonathan <jono_95@yahoo.com>
2022-10-08 13:44:49 -0400

    commit tree 3fb2958397d9fec3d441238101ebd5260ecb42fe

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+require 'pry'
 
+
+class CashRegister
+    attr_accessor :discount, :total
+
+    def initialize(discount)
+        @discount = discount
+        @total = 0
+    end
+
+end
+binding.pry
